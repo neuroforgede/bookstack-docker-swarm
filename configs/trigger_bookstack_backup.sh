ea3303746137f9255e49f89891023d59c2dc97e0
Martin Braun <braun@neuroforge.de>
2022-08-30 16:32:48 +0200

    Update trigger_bookstack_backup.sh

--- configs/trigger_bookstack_backup.sh
+++ configs/trigger_bookstack_backup.sh
@@ -3,8 +3,11 @@
 service=bookstack_bookstack
 task="bash /bookstack_backup.sh"
 
-
 matchedContainers=$(docker ps -q -f "label=com.docker.swarm.service.name=$service" | head -n1)
 lineCount=$(echo "$matchedContainers" | awk 'NF' | wc -l)
 
-docker exec -u 0 $matchedContainers $task
+if [ "$lineCount" -eq "1" ]; then
+    docker exec -u 0 $matchedContainers $task
+else
+    echo "did not find a container running service $service"
+fi
